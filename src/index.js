4a13f0c4931a97832ae61dcb38f475c00fd8b725
hanbaoyi <604594231@qq.com>
2019-12-18 17:39:24 +0800

    改写Square为函数组件

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css';
 
-class Square extends React.Component {
-	render() {
-		return (
-			<button
-				className="square"
-				onClick={() => this.props.onClick()}
-			>
-				{this.props.value}
-			</button>
-		);
-	}
+function Square({value, onClick}) {
+	return (
+		<button className="square" onClick={onClick}>
+			{value}
+		</button>
+	);
 }
 
 class Board extends React.Component {
